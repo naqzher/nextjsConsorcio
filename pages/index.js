707cc430757cd0c9e342a10ea4d907cbbb25bc58
naqzher <naqzher@gmail.com>
2019-05-05 20:29:54 -0300

    estructura nextjs

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+// const Index = () => (
+//   <div>
+//     <p>Hello Next.js</p>
+//   </div>
+// )
+
+// export default Index
+
+export default function About() {
+  return (
+    <div>
+      <p>This is the about page</p>
+    </div>
+  )
+}
